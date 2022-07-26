ee0f8ef49e95c351678a9ebc1ab544f0279aab2d
AECastelblancoV <84470602+AECastelblancoV@users.noreply.github.com>
2022-07-26 11:47:07 -0500

    Update TestS.py

---
+++ TestS.py
@@ -0,0 +1,3 @@
+import datetime
+fecha = str(datetime.datetime.now())+str('test.log')
+print (fecha)--> 2018-10-30 09:18:29.606405test.loghttps://github.com/AECastelblancoV/Test2AA/blob/main/TestS.py
